dbec9104f606fc7d08ac0e4127ba3cf10a98ec63
matumoto1234 <matumotosizuoka@gmail.com>
2021-07-03 13:52:09 +0900

    fix bug

--- math/modpow.cpp
+++ math/modpow.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 template <typename T>
 T mod_power(T a, T e, T p) {
+  if ( p <= 1 ) return 0;
   if ( e == 0 ) return 1;
   if ( e % 2 == 0 ) {
     T res = mod_power(a, e / 2, p);
--- math/pow.cpp
+++ math/pow.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 template <typename T>
 T power(T a, T e, T p = 0) {
+  if ( p <= 1 ) return 0;
   if ( p == 0 ) p = numeric_limits<T>::max();
   T res = 1;
   while ( e > 0 ) {
