6dcf48e82e436614c7686bb9bca0427716780826
matumoto1234 <matumotosizuoka@gmail.com>
2021-07-07 18:13:56 +0900

    fix format

--- graph/convertgraph.cpp
+++ graph/convertgraph.cpp
@@ -1,17 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> convert_graph(const vector<string> &s){
-  int h=s.size(),w=s[0].size();
-  const int dy[]={1,0,-1,0},dx[]={0,1,0,-1};
-  vector<vector<int>> res(h*w);
-  for(int i=0;i<h;i++){
-    for(int j=0;j<w;j++){
-      int from=i*h+j;
-      for(int k=0;k<4;k++){
-        int ny=i+dy[k],nx=j+dx[k];
-        if(ny<0||nx<0||h<=ny||w<=nx) continue;
-        int to=ny*h+j;
+// grid graph -> adjacency list
+vector<vector<int>> convert_graph(const vector<string> &s) {
+  int h = s.size(), w = s[0].size();
+  constexpr int dy[] = { 1, 0, -1, 0 }, dx[] = { 0, 1, 0, -1 };
+  vector<vector<int>> res(h * w);
+  for ( int i = 0; i < h; i++ ) {
+    for ( int j = 0; j < w; j++ ) {
+      int from = i * w + j;
+      for ( int k = 0; k < 4; k++ ) {
+        int ny = i + dy[k], nx = j + dx[k];
+        if ( ny < 0 || nx < 0 || ny >= h || nx >= w ) continue;
+        int to = ny * w + j;
         res[from].push_back(to);
       }
     }
@@ -19,20 +20,20 @@ vector<vector<int>> convert_graph(const vector<string> &s){
   return res;
 }
 
-int main(){
-  int h,w;
-  cin>>h>>w;
+int main() {
+  int h, w;
+  cin >> h >> w;
   vector<string> s(h);
-  for(int i=0;i<h;i++){
-    cin>>s[i];
+  for ( int i = 0; i < h; i++ ) {
+    cin >> s[i];
   }
 
-  auto G=convert_graph(s);
-  for(int i=0;i<h*w;i++){
-    cout<<"i:"<<i<<' ';
-    for(int to:G[i]){
-      cout<<to<<' ';
+  auto G = convert_graph(s);
+  for ( int i = 0; i < h * w; i++ ) {
+    cout << "i:" << i << ' ';
+    for ( int to : G[i] ) {
+      cout << to << ' ';
     }
-    cout<<'\n';
+    cout << '\n';
   }
 }
