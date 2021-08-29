5e76c8d85a64d5a8eac9284af603654e7844e1de
matumoto1234 <matumotosizuoka@gmail.com>
2021-08-30 04:27:39 +0900

    add return value with build() for compress in one line

--- tools/compress.cpp
+++ tools/compress.cpp
@@ -8,19 +8,29 @@ struct compress {
   compress(int N) : xs(N, 0) {}
   compress(const vector<T> &vs) : xs(vs) {}
 
+  void set(int i, T x) { xs[i] = x; }
+
+  void set(const vector<T> &vs) {
+    for ( int i = 0; i < min<int>(xs.size(), vs.size()); i++ ) {
+      xs[i] = vs[i];
+    }
+  }
+
   void add(T x) { xs.emplace_back(x); }
+
   void add(const vector<T> &vs) {
     for ( const T &x : vs ) {
       xs.emplace_back(x);
     }
   }
 
-  void build() {
+  compress<T> build() {
     sort(xs.begin(), xs.end());
     xs.erase(unique(xs.begin(), xs.end()), xs.end());
+    return *this;
   }
 
-  vector<T> get(const vector<T> &vs) {
+  vector<T> get(const vector<T> &vs) const {
     vector<T> res = vs;
     for ( T &x : res ) {
       x = lower_bound(xs.begin(), xs.end(), x) - xs.begin();
@@ -35,9 +45,15 @@ struct compress {
   const T &operator[](int k) const { return xs[k]; }
 };
 
+// ABC036_C
 int main() {
   int n;
   cin >> n;
-  vector<int> a(n);
-  compress<int> comp(a);
+  vector<int> as(n);
+  for(auto &a:as) cin>>a;
+  
+  vector<int> compressed = compress(as).build().get(as);
+  for (auto x : compressed) {
+    cout << x << "\n";
+  }
 }
