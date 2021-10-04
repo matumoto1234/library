69d99f54945b8b285831a63532513af35a2913cf
matumoto1234 <matumotosizuoka@gmail.com>
2021-10-05 07:50:04 +0900

    update split

--- string/split.cpp
+++ string/split.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 vector<string> split(const string &str, const string &sep) {
   vector<string> res;
   string t = "";
@@ -12,6 +11,24 @@ vector<string> split(const string &str, const string &sep) {
     }
     res.emplace_back(t);
     t = "";
+  }
+  if ( t != "" ) res.emplace_back(t);
+  return res;
+}
+
+vector<string> split(const string &str, int k) {
+  assert(k >= 1);
+  int cnt = 0;
+  vector<string> res;
+  string t = "";
+  for ( auto c : str ) {
+    cnt++;
+    t += c;
+    if ( cnt >= k ) {
+      res.emplace_back(t);
+      cnt = 0;
+      t = "";
+    }
   }
   if ( t != "" ) res.emplace_back(t);
   return res;
