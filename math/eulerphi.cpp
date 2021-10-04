689aefd0e81acaa3e07cab9fa998747668391d1c
matumoto1234 <matumotosizuoka@gmail.com>
2021-10-05 07:42:31 +0900

    fix euler phi

--- math/eulerphi.cpp
+++ math/eulerphi.cpp
@@ -1,20 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-long long euler_phi(long long n){
-  long long res=n;
-  for(long long i=2;i*i<=n;i++){
-    if(n%i==0){
-      res-=res/i;
-      while(n%i==0) n/=i;
+long long euler_phi(long long n) {
+  long long res = n;
+  for ( long long i = 2; i * i <= n; i++ ) {
+    if ( n % i == 0 ) {
+      res -= res / i;
+      while ( n % i == 0 )
+        n /= i;
     }
   }
-  if(n>1) res-=res/n;
+  if ( n > 1 ) res -= res / n;
   return res;
 }
 
-int main(){
+int main() {
   long long n;
-  cin>>n;
-  cout<<euler_phi(n)<<endl;
+  cin >> n;
+  cout << euler_phi(n) << endl;
 }
