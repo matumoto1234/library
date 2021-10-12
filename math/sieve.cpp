8ee3d18059495dee6d144407a63f3a0cdea901bd
matumoto1234 <matumotosizuoka@gmail.com>
2021-10-13 07:48:25 +0900

    add sieve

--- math/sieve.cpp
+++ math/sieve.cpp
@@ -1,16 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct sieve {
+struct eratosthenes_sieve {
   vector<bool> prime_table;
 
-  sieve(int N) : prime_table(N + 1, true) {
+  eratosthenes_sieve() {}
+  eratosthenes_sieve(int N) : prime_table(N + 1, true) {
     prime_table[0] = prime_table[1] = false;
   }
 
   void build() {
     int n = prime_table.size();
-    for ( long long i = 2; i < n; i++ ) {
+    for ( long long i = 2; i * i < n; i++ ) {
       if ( !prime_table[i] ) continue;
       for ( long long j = i * i; j < n; j += i ) {
         prime_table[j] = false;
@@ -20,3 +21,17 @@ struct sieve {
 
   bool operator[](int k) { return prime_table[k]; }
 };
+
+int main(){
+  int n;
+  cin>>n;
+  eratosthenes_sieve sieve(n);
+  sieve.build();
+
+  for(int i=0;i<(int)sieve.prime_table.size();i++){
+    if(sieve.prime_table[i]){
+      cout<<i<<' ';
+    }
+  }
+  cout<<endl;
+}
