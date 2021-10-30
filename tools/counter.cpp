1ebc36c9629b6d8f16bebc2422e0197adf05798d
matumoto1234 <matumotosizuoka@gmail.com>
2021-10-31 04:40:44 +0900

    Update iteratable

--- tools/counter.cpp
+++ tools/counter.cpp
@@ -1,13 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T>
-map<T, int> counter(const vector<T> &vs) {
+template <typename Iterator>
+auto counter(Iterator first, Iterator last) {
+  using T = remove_reference_t<decltype(*first)>;
   map<T, int> res;
-  for ( T v : vs ) res[v]++;
+  for (auto it = first; it != last; it++) {
+    T value = *it;
+    res[value]++;
+  }
   return res;
 }
 
 int main() {
-	
+  {
+    vector<int> a = { 2, 4, 4, 3, 5, 5, 5, 5 };
+    auto mp = counter(a.begin(), a.end());
+    for (auto [val, cnt]: mp) {
+      cout << val << ' ' << cnt << endl;
+    }
+  }
+  {
+    string s = "aaabbbcccccd";
+    auto mp = counter(s.begin(), s.end());
+    for (auto [val, cnt]: mp) {
+      cout << val << ' ' << cnt << endl;
+    }
+  }
 }
