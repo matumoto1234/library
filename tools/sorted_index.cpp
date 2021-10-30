2457b1a57f0825e9fdc567da0cecb66411ae5bc2
matumoto1234 <matumotosizuoka@gmail.com>
2021-10-31 04:42:31 +0900

    Refactored

--- tools/sorted_index.cpp
+++ tools/sorted_index.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename iter>
-vector<int> sorted_index(const iter &first, const iter &last) {
-  auto tmp = *first;
-  vector<decltype(tmp)> a(first, last);
+template <typename Iterator>
+vector<int> sorted_index(Iterator first, Iterator last) {
+  using T = remove_reference_t<decltype(*first)>;
+  vector<T> a(first, last);
   vector<int> res(a.size());
   iota(res.begin(), res.end(), 0);
   stable_sort(res.begin(), res.end(), [&](int i, int j) {
@@ -17,7 +17,7 @@ int main() {
   vector<int> a = { 3, 1, 4, 1, 5 };
   auto idxs = sorted_index(a.begin(), a.end());
   // expected : 1, 3, 0, 2, 4
-  for ( auto idx : idxs ) {
+  for (auto idx: idxs) {
     cout << idx << ' ';
   }
   cout << endl;
