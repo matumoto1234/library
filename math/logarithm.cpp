d7dcb536eb0392f8a577537f22fdaef589e72abd
matumoto1234 <matumotosizuoka@gmail.com>
2021-10-31 03:28:18 +0900

    add logarithm

---
+++ math/logarithm.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int logarithm(ll base, ll n) {
+  assert(base != 0);
+  int cnt = 0;
+  while (n % base == 0) {
+    n /= base;
+    cnt++;
+  }
+  return cnt;
+}
+
+int main(){
+
+}
