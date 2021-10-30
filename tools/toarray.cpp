8a043ce2b8de1a26056d161db34ace93d601c03c
matumoto1234 <matumotosizuoka@gmail.com>
2021-10-31 04:42:45 +0900

    Refactored

--- tools/toarray.cpp
+++ tools/toarray.cpp
@@ -1,16 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 template <typename T, size_t length>
 array<T, length> to_array(const vector<T> &vs) {
   array<T, length> res;
-  for ( int i = 0; i < (int)length; i++ ) {
+  for (int i = 0; i < (int)length; i++) {
     res[i] = vs[i];
   }
   return res;
 }
 
 int main() {
-
 }
