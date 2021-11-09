aaf2713100fe3747d55f0ce3b35466d4a2cdef05
matumoto1234 <matumotosizuoka@gmail.com>
2021-11-10 02:01:36 +0900

    Add base.cpp

---
+++ geometry/base.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <cmath>
+
+namespace geometry {
+  using namespace std;
+  using Real = double;
+
+  constexpr Real PI = M_PI;
+  constexpr Real EPS = 1e-9;
+
+  inline int sign(Real a) {
+    if (a < -EPS) return -1;
+    if (a > +EPS) return +1;
+    return 0;
+  }
+
+  inline bool equals(Real a, Real b) { return sign(a - b) == 0; }
+} // namespace geometry
