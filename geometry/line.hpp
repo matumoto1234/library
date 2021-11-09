5d9ecdb81c62e597d268fd8e3493beec530303da
matumoto1234 <matumotosizuoka@gmail.com>
2021-11-10 02:02:37 +0900

    Add line.hpp

---
+++ geometry/line.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "./point.hpp"
+
+namespace geometry {
+  struct Line {
+    Point a, b;
+
+    Line() = default;
+    Line(const Point &a, const Point &b) : a(a), b(b) {}
+  };
+} // namespace geometry
