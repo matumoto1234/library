a849ee551d856768ab2663fe11a15daddc1a6acb
matumoto1234 <matumotosizuoka@gmail.com>
2021-11-10 02:02:24 +0900

    Add projection.hpp

---
+++ geometry/projection.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "./base.hpp"
+#include "./point.hpp"
+#include "./line.hpp"
+#include "./product.hpp"
+
+namespace geometry {
+  Point projection(const Line &l, const Point &p) {
+    // Real 
+  }
+}
