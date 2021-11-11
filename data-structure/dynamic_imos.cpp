b16d312183f6a138388820e436e6f61ec45f8c44
matumoto1234 <matumotosizuoka@gmail.com>
2021-11-11 10:54:55 +0900

    Update dynamic_imos, #7, #11

--- data-structure/dynamic_imos.cpp
+++ data-structure/dynamic_imos.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 
 template <typename T>
-struct dynamic_imos {
+struct DynamicImos {
   vector<T> xs, imos;
-  vector<tuple<T, T, T>> sections;
+  vector<tuple<T, T, T>> intervals;
 
-  dynamic_imos() {}
+  DynamicImos() {}
 
   // [l, r)
   void add(T l, T r, T v) {
-    sections.emplace_back(l, r, v);
+    intervals.emplace_back(l, r, v);
     xs.emplace_back(l);
     xs.emplace_back(r);
   }
@@ -21,26 +21,26 @@ struct dynamic_imos {
     xs.erase(unique(xs.begin(), xs.end()), xs.end());
     imos.assign(xs.size(), 0);
 
-    for ( auto [l, r, v] : sections ) {
+    for ( auto [l, r, v] : intervals ) {
       l = lower_bound(xs.begin(), xs.end(), l) - xs.begin();
       r = lower_bound(xs.begin(), xs.end(), r) - xs.begin();
       imos[l] += v;
       imos[r] -= v;
     }
 
-    for ( int i = 1; i < (int)imos.size(); i++ ) {
+    for ( int i = 1; i < static_cast<int>(imos.size()); i++ ) {
       imos[i] += imos[i - 1];
     }
   }
 
   // vector<[l,r), value>
-  vector<pair<pair<T, T>, T>> section_values() {
+  vector<pair<pair<T, T>, T>> interval_values() {
     vector<pair<pair<T, T>, T>> res(xs.size() - 1);
-    for ( int i = 0; i < (int)xs.size() - 1; i++ ) {
+    for ( int i = 0; i < static_cast<int>(xs.size()) - 1; i++ ) {
       T l = xs[i];
       T r = xs[i + 1];
       T v = imos[i];
-      res[i] = make_pair(make_pair(l, r), v);
+      res[i] = pair(pair(l, r), v);
     }
     return res;
   }
