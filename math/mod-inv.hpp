d3fb2d3142d6ce52285122f2a83b3108b940f076
matumoto1234 <matumotosizuoka@gmail.com>
2021-11-14 22:57:51 +0900

    Split fuction, #34

--- math/mod-inv.hpp
+++ math/mod-inv.hpp
@@ -1,19 +1,9 @@
 #pragma once
 
 #include "./base.hpp"
+#include "./extgcd.hpp"
 
 namespace math {
-  ll extgcd(ll a, ll b, ll &x, ll &y) {
-    if (b == 0) {
-      x = 1;
-      y = 0;
-      return a;
-    }
-    ll d = extgcd(b, a % b, y, x);
-    y = y - (a / b) * x;
-    return d;
-  }
-
   ll modinv(ll n, ll mod) {
     ll x, y;
     extgcd(n, mod, x, y);
