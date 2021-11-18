1ccc8c8f701bc8622fa44b6d0700742a6d736815
matumoto1234 <matumotosizuoka@gmail.com>
2021-11-18 11:42:28 +0900

    Update euler-tour.hpp, #61

--- graph/euler-tour.hpp
+++ graph/euler-tour.hpp
@@ -6,22 +6,21 @@
 
 namespace graph {
   class EulerTour {
-  private:
-    vector<int> _in, _out, _depth, _parent, _edge_table;
+    vector<int> in_time, out_time, dep, par, edge_table_;
     vector<vector<int>> G;
 
-    void dfs(int v, int &time, int dep) {
-      _in[v] = time;
-      _depth[v] = dep;
-      _edge_table[time] = v;
+    void dfs(int v, int &time, int d) {
+      in_time[v] = time;
+      dep[v] = d;
+      edge_table_[time] = v;
       time++;
       for (auto to: G[v]) {
-        if (_in[to] != -1) continue;
-        _parent[to] = v;
-        dfs(to, time, dep + 1);
+        if (in_time[to] != -1) continue;
+        par[to] = v;
+        dfs(to, time, d + 1);
       }
-      _out[v] = time;
-      _edge_table[time] = -v;
+      out_time[v] = time;
+      edge_table_[time] = -v;
       time++;
     }
 
@@ -31,29 +30,29 @@ namespace graph {
     void add_edge(int from, int to) { G[from].emplace_back(to); }
 
     void build(int s) {
-      _in.assign(G.size(), -1);
-      _out.assign(G.size(), -1);
-      _depth.assign(G.size(), -1);
-      _parent.assign(G.size(), -1);
-      _edge_table.assign(2 * G.size(), -1);
+      in_time.assign(G.size(), -1);
+      out_time.assign(G.size(), -1);
+      dep.assign(G.size(), -1);
+      par.assign(G.size(), -1);
+      edge_table_.assign(2 * G.size(), -1);
 
       int time = 0;
       dfs(s, time, 0);
     }
 
-    // edge_table:[first, second), vertex_table:[first, second]
-    pair<int, int> subtree(int v) { return make_pair(_in[v], _out[v]); }
+    int in(int v) { return in_time[v]; }
+    int out(int v) { return out_time[v]; }
+    int depth(int v) { return dep[v]; }
+    int parent(int v) { return par[v]; }
+    vector<int> edge_table() { return edge_table_; }
 
-    vector<int> in() { return _in; }
-    vector<int> out() { return _out; }
-    vector<int> depth() { return _depth; }
-    vector<int> parent() { return _parent; }
-    vector<int> edge_table() { return _edge_table; }
+    // edge_table:[first, second), vertex_table:[first, second]
+    pair<int, int> subtree(int v) { return pair(in_time[v], out_time[v]); }
 
     vector<int> vertex_table() {
-      vector<int> res = _edge_table;
+      vector<int> res = edge_table_;
       for (auto &v: res) {
-        if (v < 0) v = _parent[v];
+        if (v < 0) v = par[v];
       }
       // 半開区間->閉区間の誤差
       res.pop_back();
