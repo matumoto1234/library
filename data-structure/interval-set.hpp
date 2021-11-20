768b3b56e3cfbf06c335340cd25d07f82c3c3f31
matumoto1234 <matumotosizuoka@gmail.com>
2021-11-20 23:23:14 +0900

    Update interval-set, #59

--- data-structure/interval-set.hpp
+++ data-structure/interval-set.hpp
@@ -2,35 +2,39 @@
 
 #include "./base.hpp"
 
+#include <cassert>
 #include <set>
 
 namespace data_structure {
-  // verify:ABC217_D
+  // verify:ABC228_D
   template <typename T = long long>
-  struct PairSet {
+  struct IntervalSet {
     set<pair<T, T>> s;
 
-    PairSet() {}
+    IntervalSet() {
+      s.emplace(-inf(), -inf());
+      s.emplace(inf(), inf());
+    }
 
-    T inf() { return numeric_limits<T>::max() / 2; }
+    T inf() const { return numeric_limits<T>::max() / 2; }
 
-    bool empty() const { return s.empty(); }
+    bool empty() const { return static_cast<int>(s.size()) == 2; }
 
     bool contains(T x) const {
-      if (s.empty()) return false;
-      auto [lower, upper] = *prev(s.lower_bound(make_pair(x + 1, x + 1)));
+      if (empty()) return false;
+      auto [lower, upper] = *prev(s.lower_bound(pair(x + 1, x + 1)));
       return lower <= x and x <= upper;
     }
 
     // max({ (lower, upper) | lower <= x <= upper })
-    pair<T, T> get(T x) {
-      if (!contains(x)) return make_pair<T, T>(inf(), inf());
-      return *prev(s.lower_bound(make_pair(x + 1, x + 1)));
+    pair<T, T> get(T x) const {
+      assert(contains(x));
+      return *prev(s.lower_bound(pair(x + 1, x + 1)));
     }
 
     bool insert(T x) {
       if (contains(x)) return false;
-      auto nit = s.lower_bound(make_pair(x + 1, x + 1));
+      auto nit = s.lower_bound(pair(x + 1, x + 1));
       auto it = prev(nit);
       auto [l, u] = *it;
       auto [nl, nu] = *nit;
@@ -52,29 +56,29 @@ namespace data_structure {
 
     bool insert(T x, T y) {
       if (x > y) swap(x, y);
-      bool xflg = contains(x);
-      bool yflg = contains(y);
-      if (xflg and yflg) {
-        auto xit = prev(s.lower_bound(make_pair(x + 1, x + 1)));
-        auto yit = prev(s.lower_bound(make_pair(y + 1, y + 1)));
+      bool contains_x = contains(x);
+      bool contains_y = contains(y);
+      if (!contains_x and !contains_y) {
+        s.emplace(x, y);
+      } else if (!contains_x and contains_y) {
+        auto yit = prev(s.lower_bound(pair(y + 1, y + 1)));
+        auto [yl, yu] = *yit;
+        s.erase(yit);
+        s.emplace(x, yu);
+      } else if (contains_x and !contains_y) {
+        auto xit = prev(s.lower_bound(pair(x + 1, x + 1)));
+        auto [xl, xu] = *xit;
+        s.erase(xit);
+        s.emplace(xl, y);
+      } else {
+        auto xit = prev(s.lower_bound(pair(x + 1, x + 1)));
+        auto yit = prev(s.lower_bound(pair(y + 1, y + 1)));
         if (xit == yit) return false;
         auto [xl, xu] = *xit;
         auto [yl, yu] = *yit;
         s.erase(xit);
         s.erase(yit);
         s.emplace(xl, yu);
-      } else if (xflg and !yflg) {
-        auto xit = prev(s.lower_bound(make_pair(x + 1, x + 1)));
-        auto [xl, xu] = *xit;
-        s.erase(xit);
-        s.emplace(xl, y);
-      } else if (!xflg and yflg) {
-        auto yit = prev(s.lower_bound(make_pair(y + 1, y + 1)));
-        auto [yl, yu] = *yit;
-        s.erase(yit);
-        s.emplace(x, yu);
-      } else if (!xflg and !yflg) {
-        s.emplace(x, y);
       }
       return true;
     }
@@ -83,8 +87,9 @@ namespace data_structure {
 
     // { (l, u) | l <= x <= u } => { (l, x - 1), (x + 1, u) | l <= x <= u }
     bool erase(T x) {
+      assert(x != -inf() and x != inf());
       if (!contains(x)) return false;
-      auto it = prev(s.lower_bound(make_pair(x + 1, x + 1)));
+      auto it = prev(s.lower_bound(pair(x + 1, x + 1)));
       auto [l, u] = *it;
       s.erase(it);
       if (x != l) s.emplace(l, x - 1);
@@ -94,8 +99,9 @@ namespace data_structure {
 
     // { (l, u) | l <= x <= u } => { (l, x), (x, u) | l <= x <= u }
     bool cut(T x) {
+      assert(x != -inf() and x != inf());
       if (!contains(x)) return false;
-      auto it = prev(s.lower_bound(make_pair(x + 1, x + 1)));
+      auto it = prev(s.lower_bound(pair(x + 1, x + 1)));
       auto [l, u] = *it;
       s.erase(it);
       s.emplace(l, x);
