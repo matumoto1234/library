7026f67bd16dac36c2fad3e439ab9c4eb741dd1b
matumoto1234 <matumotosizuoka@gmail.com>
2021-12-06 09:40:20 +0900

    Add stopwatch.hpp, #92

---
+++ tools/stopwatch.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "./base.hpp"
+
+#include <chrono>
+
+namespace tools {
+  struct Stopwatch {
+    chrono::high_resolution_clock::time_point start;
+
+    Stopwatch() { restart(); }
+
+    void restart() { start = chrono::high_resolution_clock::now(); }
+
+    chrono::milliseconds::rep elapsed() {
+      auto end = chrono::high_resolution_clock::now();
+      return chrono::duration_cast<chrono::milliseconds>(end - start).count();
+    }
+  };
+} // namespace tools
