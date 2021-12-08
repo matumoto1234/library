a264e5bd133936b424668ac0d27ac60e3f699135
matumoto1234 <matumotosizuoka@gmail.com>
2021-12-08 11:07:07 +0900

    Add has_find_v, #99

--- tools/has-find.hpp
+++ tools/has-find.hpp
@@ -3,17 +3,12 @@
 #include "./has-iterator.hpp"
 
 namespace tools {
+  template <typename Container, typename T, enable_if_t<(static_cast<has_iterator_t<Container> (Container::*)(const T &)>(&Container::find), true), nullptr_t> = nullptr>
+  class HasFind: public true_type {};
+
   template <typename Container, typename T>
-  class HasFind {
-    template <typename InnerContainer>
-    using Iterator = typename enable_if<HasIterator<InnerContainer>::value, InnerContainer>::type::iterator;
+  class HasFind: public false_type {};
 
-    template <typename InnerContainer, int dummy = (static_cast<Iterator<InnerContainer> (InnerContainer::*)(const T &)>(&InnerContainer::find), 0)>
-    static true_type check(InnerContainer *);
-    static false_type check(...);
-    static Container *container;
-
-  public:
-    static const bool value = decltype(check(container))::value;
-  };
+  template <typename Container, typename T>
+  using has_find_v = HasFind<Container, T>::value;
 } // namespace tools
