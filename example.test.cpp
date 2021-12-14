9b1c116f0a62bd63a1e7ea33be56978c8278c1fd
matumoto1234 <matumotosizuoka@gmail.com>
2021-12-14 11:15:20 +0900

    Format test, #101

--- example.test.cpp
+++ example.test.cpp
@@ -6,4 +6,4 @@
 #define ALL(x) std::begin(x), std::end(x)
 using namespace std;
 
-int main() {int n;cin>>n;vector<int> s(n);REP(i,n) {cin >> s[i]; } int q; cin >> q; int cnt = 0; while (q--) { int t_i; cin >> t_i; cnt += binary_search(ALL(s), t_i); } cout << cnt << endl; return 0; }
+int main() { int n; cin >> n; vector<int> s(n); REP(i, n) { cin >> s[i]; } int q; cin >> q; int cnt = 0; while (q--) { int t_i; cin >> t_i; cnt += binary_search(ALL(s), t_i); } cout << cnt << endl; return 0; }
