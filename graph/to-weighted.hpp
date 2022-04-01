009815cece28f4f67a2939cb6ad564af324672e1
matumoto1234 <matumotosizuoka@gmail.com>
2022-04-01 16:36:31 +0900

    #141:Add to-weighted

---
+++ graph/to-weighted.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "./base.hpp"
+#include "graph-type.hpp"
+
+namespace graph_library {
+  template <typename Cost>
+  WeightedGraph<Cost> to_weighted(const UnWeightedGraph &graph, const Cost &init) {
+    WeightedGraph<Cost> res(graph.size());
+
+    const UnWeightedEdges &edges = graph.edges();
+    for (const UnWeightedEdge &edge: edges) {
+      WeightedEdge<Cost> weighted_edge(edge.from(), edge.to(), init);
+      res.add_edge(weighted_edge);
+    }
+
+    return res;
+  }
+} // namespace graph_library
