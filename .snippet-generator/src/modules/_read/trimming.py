f1b5564291f994e2fbe9979b1d012daed6fe9a71
matumoto1234 <matumotosizuoka@gmail.com>
2022-06-27 10:12:48 +0900

    #153:fix!

--- .snippet-generator/src/modules/_read/trimming.py
+++ .snippet-generator/src/modules/_read/trimming.py
@@ -3,13 +3,12 @@ from typing import List
 # trimming empty lines from the beginning of list
 def trimming_empty_lines(lines: List[str]) -> List[str]:
     idx = 0
-    while lines[idx] == '':
-
-        # if the file includes only empty lines
-        if idx == len(lines):
-            return []
-
+    while idx < len(lines) and lines[idx] == '':
         idx += 1
+
+    # if the file includes only empty lines
+    if idx == len(lines):
+        return []
 
     result_lines: List[str] = []
 
