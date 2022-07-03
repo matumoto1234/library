244ce26fefe3e6f3836e18ff12441de993dc6c23
matumoto1234 <matumotosizuoka@gmail.com>
2022-07-04 00:36:10 +0900

    #172:fix convert-graph

--- graph/convert-graph.hpp
+++ graph/convert-graph.hpp
@@ -1,28 +1,35 @@
 #pragma once
 
 #include "./base.hpp"
+#include "./graph-type.hpp"
 
 #include <string>
-#include <vector>
 
 namespace graph_library {
-  // grid graph -> adjacency list
-  vector<vector<int>> convert_graph(const vector<string> &s) {
+  // grid graph -> UnWeightedGraph
+  UnWeightedGraph convert_graph(const vector<string> &s, const string &wall = "#") {
     int h = s.size(), w = s[0].size();
     constexpr int dy[] = { 1, 0, -1, 0 }, dx[] = { 0, 1, 0, -1 };
-    vector<vector<int>> res(h * w);
+    UnWeightedGraph graph(h * w);
+
     for (int i = 0; i < h; i++) {
       for (int j = 0; j < w; j++) {
         int from = i * w + j;
+
         for (int k = 0; k < 4; k++) {
           int ny = i + dy[k], nx = j + dx[k];
+
           if (ny < 0 or nx < 0 or ny >= h or nx >= w)
             continue;
-          int to = ny * w + j;
-          res[from].push_back(to);
+
+          if (wall.find(s[ny][nx]) != string::npos)
+            continue;
+
+          int to = ny * w + nx;
+          graph.add_edge(UnWeightedEdge(from, to));
         }
       }
     }
-    return res;
+    return graph;
   }
 } // namespace graph_library
