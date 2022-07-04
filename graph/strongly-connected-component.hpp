cdc5d63f1d77a71b052a1b1eda5e73ff92462343
matumoto1234 <matumotosizuoka@gmail.com>
2022-07-04 14:41:14 +0900

    #172:fix scc using template

--- graph/strongly-connected-component.hpp
+++ graph/strongly-connected-component.hpp
@@ -5,19 +5,20 @@
 #include "./reverse-edges.hpp"
 
 namespace graph_library {
+  template <typename Graph>
   class StronglyConnectedComponent {
-    UnWeightedGraph graph_, scc_graph_;
+    Graph scc_graph_;
     vector<vector<int>> scc_;
     vector<bool> used_;
     vector<int> post_order_, group_;
 
   public:
-    StronglyConnectedComponent(const UnWeightedGraph &graph): graph_(graph) {
-      int n = graph_.size();
+    StronglyConnectedComponent(const Graph &graph) {
+      int n = graph.size();
 
       // get post order
       used_.assign(n, false);
-      auto adj_list = graph_.graph();
+      auto adj_list = graph.graph();
 
       for (int i = 0; i < n; i++) {
         if (used_[i])
@@ -30,8 +31,8 @@ namespace graph_library {
       group_.assign(n, 0);
       used_.assign(n, false);
 
-      UnWeightedGraph rgraph = reverse_edges(graph_);
-      vector<UnWeightedEdges> radj_list = rgraph.graph();
+      Graph rgraph = reverse_edges(graph);
+      auto radj_list = rgraph.graph();
 
       // scc grouping
       for (int i = n - 1; i >= 0; i--) {
@@ -44,7 +45,7 @@ namespace graph_library {
       }
 
       scc_.assign(scc_cnt, vector<int>(0));
-      scc_graph_ = UnWeightedGraph(scc_cnt);
+      scc_graph_ = Graph(scc_cnt);
 
       for (int i = 0; i < n; i++) {
         // build scc
@@ -56,7 +57,11 @@ namespace graph_library {
           if (is_same_scc)
             continue;
 
-          scc_graph_.add_edge(UnWeightedEdge(group_[i], group_[e.to()]));
+          if constexpr (is_same_v<Graph, UnWeightedGraph>) {
+            scc_graph_.add_edge(UnWeightedEdge(group_[i], group_[e.to()]));
+          } else {
+            scc_graph_.add_edge(WeightedEdge(group_[i], group_[e.to()], e.cost()));
+          }
         }
       }
     }
@@ -69,12 +74,13 @@ namespace graph_library {
       return scc_;
     }
 
-    UnWeightedGraph scc_graph() const {
+    Graph scc_graph() const {
       return scc_graph_;
     }
 
   private:
-    void dfs(const vector<UnWeightedEdges> &adj_list, int idx) {
+    template <typename Edges>
+    void dfs(const vector<Edges> &adj_list, int idx) {
       used_[idx] = true;
       for (const auto &e: adj_list[idx]) {
         const int &to = e.to();
@@ -86,7 +92,8 @@ namespace graph_library {
       post_order_.emplace_back(idx);
     }
 
-    void rdfs(const vector<UnWeightedEdges> &adj_list, int idx, int group_id) {
+    template <typename Edges>
+    void rdfs(const vector<Edges> &adj_list, int idx, int group_id) {
       used_[idx] = true;
       group_[idx] = group_id;
       for (const auto &e: adj_list[idx]) {
